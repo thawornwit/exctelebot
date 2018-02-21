4399546f8034474c72ef900b9a71bd81940ae5ec
thawornwit <javacpp.exp@gmail.com>
2018-02-21 07:38:42 +0700

    Remove tokens

--- tokens.py
+++
@@ -1,7 +0,0 @@
-# A token you get from the Telegram's botfather
-telegrambot='543490987:AAHQ2hm8x7G4vVwC1lMq5Su8T3I5zxdW9OM'
-# A chat_id of your client
-adminchatid=[259669700,263956990]
-#memberid=[263956990,263956990,263956990,263956990,263956990,263956990]
-memberchatid=[]
-
